0c338f5ebcbd412120a8c976416d2ad7e3d4473b
omermeushar <omermeushar@campus.technion.ac.il>
2023-06-03 19:32:17 +0300

    omer and michal 3/6: passed tests

--- HW2_submit_files/kernel/hw2.c
+++ HW2_submit_files/kernel/hw2.c
@@ -3,6 +3,9 @@
 #include <linux/list.h>
 #include <linux/module.h>
 
+struct task_struct* sys_get_heaviest_descendant_aux(struct task_struct* task);
+
+
 asmlinkage long sys_hello(void) { //333
  printk("Hello, World!\n");
  return 0;
@@ -32,20 +35,20 @@ asmlinkage long sys_get_ancestor_sum(void) { ///336
 asmlinkage long sys_get_heaviest_descendant(void) { ///337
     if(list_empty(&(current->children))
         return -ECHILD;
-    return sys_get_heaviest_descendant_aux(current)->pid;
+    return (long)((sys_get_heaviest_descendant_aux(current))->pid);
 }
 
-asmlinkage struct task_struct* sys_get_heaviest_descendant_aux(struct task_struct* task) { ///337
-    if(list_empty(&(task->children))
+struct task_struct* sys_get_heaviest_descendant_aux(struct task_struct* task) { ///337
+    if(list_empty(&(task->children)))
         return NULL;
     struct list_head* head = &(task->children);
     struct list_head* curr_node;
     struct task_struct* curr_task;
     struct task_struct* max_task = NULL;
-    list_for_each(curr_node, head) {
+    list_for_each(curr_node, head){
         curr_task = list_entry(curr_node, struct task_struct, sibling);
-        struct task_struct* max_task_in_subtree = sys_get_heaviest_descendant_aux(curr_task->children);
-        if (curr_task->weight > max_task_in_subtree->weight || (curr_task->weight == max_task_in_subtree->weight && curr_task->pid < max_task_in_subtree->pid))
+        struct task_struct* max_task_in_subtree = sys_get_heaviest_descendant_aux(curr_task);
+        if (max_task_in_subtree == NULL || (curr_task->weight > max_task_in_subtree->weight || (curr_task->weight == max_task_in_subtree->weight && curr_task->pid < max_task_in_subtree->pid)))
             max_task_in_subtree = curr_task;
         if (max_task->weight > max_task_in_subtree->weight || (max_task->weight == max_task_in_subtree->weight && max_task->pid < max_task_in_subtree->pid))
             max_task = max_task_in_subtree;
