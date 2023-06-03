7ab39d2d04e11b5f5e595a976df9a959dc59117d
omermeushar <omermeushar@campus.technion.ac.il>
2023-06-03 19:32:30 +0300

    omer and michal 3/6: passed tests

--- HW2_submit_files/include/linux/sched.h
+++ HW2_submit_files/include/linux/sched.h
@@ -1135,8 +1135,7 @@ struct task_struct {
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-
-    long weight
+    long weight;
 	randomized_struct_fields_end
 
 	/* CPU-specific state of this task: */
--- HW2_submit_files/kernel/hw2.c
+++ HW2_submit_files/kernel/hw2.c
@@ -33,7 +33,7 @@ asmlinkage long sys_get_ancestor_sum(void) { ///336
 }
 
 asmlinkage long sys_get_heaviest_descendant(void) { ///337
-    if(list_empty(&(current->children))
+    if(list_empty(&(current->children)))
         return -ECHILD;
     return (long)((sys_get_heaviest_descendant_aux(current))->pid);
 }
@@ -50,9 +50,8 @@ struct task_struct* sys_get_heaviest_descendant_aux(struct task_struct* task) { 
         struct task_struct* max_task_in_subtree = sys_get_heaviest_descendant_aux(curr_task);
         if (max_task_in_subtree == NULL || (curr_task->weight > max_task_in_subtree->weight || (curr_task->weight == max_task_in_subtree->weight && curr_task->pid < max_task_in_subtree->pid)))
             max_task_in_subtree = curr_task;
-        if (max_task->weight > max_task_in_subtree->weight || (max_task->weight == max_task_in_subtree->weight && max_task->pid < max_task_in_subtree->pid))
+        if (max_task == NULL || (max_task->weight < max_task_in_subtree->weight || (max_task->weight == max_task_in_subtree->weight && max_task->pid > max_task_in_subtree->pid)))
             max_task = max_task_in_subtree;
     }
     return max_task;
-
 }
